6d8eba57ece7f8d2f30fdc5fc60d579531b4e844
Oksana <ksenia.barshchyk@yandex.ru>
2017-07-27 15:19:05 +0300

    Added 2 hours dalay

--- break_time.py
+++ break_time.py
@@ -1,2 +1,13 @@
 # open a public URL, in this case, the webbrowser docs
-webbrowser.open('https://www.youtube.com/watch?v=w-Ng5muAAcg&list=RDw-Ng5muAAcg#t=3')
+import webbrowser
+import time
+
+total_breaks = 3
+break_count = 0
+
+print("This program started "+time.ctime())
+while (break_count < total_breaks):
+    time.sleep(2*60*60) #added 2 hours dalay
+    webbrowser.open('https://www.youtube.com/watch?v=w-Ng5muAAcg&list=RDw-Ng5muAAcg#t=3')
+    break_count = break_count + 1
+
