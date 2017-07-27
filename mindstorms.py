efccea7e3967b45f5df0dd329201264cf4feb51e
Oksana <ksenia.barshchyk@yandex.ru>
2017-07-27 21:15:32 +0300

    print the rectangle

--- mindstorms.py
+++ mindstorms.py
@@ -4,6 +4,9 @@ def draw_square():
     window = turtle.Screen()
     window.bgcolor("red")
     brad = turtle.Turtle()
+    brad.shape('turtle')
+    brad.color('yellow')
+    brad.speed(2)
 
     brad.forward(100) #go forward 100px
     brad.right(90) # go right 90px
@@ -17,6 +20,18 @@ def draw_square():
     brad.forward(100)
     brad.right(90)
 
+    angie = turtle.Turtle()
+    angie.shape('arrow')
+    angie.color('blue')
+    angie.circle(100)
+
+    triangle = turtle.Turtle()
+    triangle.shape('turtle')
+    triangle.color('green')
+    triangle.forward(100)
+    triangle.right(90)
+    triangle.home()
+    triangle.position()
 
     window.exitonclick()
 
