31f1b94a03e815f2c3a5fba26e87b88224cd0826
Oksana <ksenia.barshchyk@yandex.ru>
2017-07-27 16:26:17 +0300

    added print filenames

--- secret_message.py
+++ secret_message.py
@@ -1,6 +1,5 @@
 import os
 
-
 def rename_files():
     #1 stes - get files from the folder
     file_list = os.listdir(r"/Users/ione/Downloads/prank")
@@ -9,7 +8,7 @@ def rename_files():
     saved_path = os.getcwd()
     os.chdir(r"/Users/ione/Downloads/prank")
     print ("Current directory is"+ saved_path)
-    #for each file rename the filename
+    #2 step - for each file rename the filename
     for filename in file_list:
         os.rename(filename, filename.translate(None, "1234567890"))
         print("Old names"+filename)
