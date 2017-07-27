aa1b806a1cb92f25802a87cffe2a7f95d7038356
Oksana <ksenia.barshchyk@yandex.ru>
2017-07-27 16:12:12 +0300

    Added secret message program which rename file names

---
+++ secret_message.py
@@ -0,0 +1,17 @@
+import os
+
+
+def rename_files():
+    #1 stes - get files from the folder
+    file_list = os.listdir(r"/Users/ione/Downloads/prank")
+    print(file_list)
+
+    saved_path = os.getcwd()
+    os.chdir(r"/Users/ione/Downloads/prank")
+    print ("Current directory is"+ saved_path)
+    #for each file rename the filename
+    for filename in file_list:
+        os.rename(filename, filename.translate(None, "1234567890"))
+    os.chdir(saved_path)
+
+rename_files()
