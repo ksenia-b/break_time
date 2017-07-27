eb323a4e224476e14c0ed954c13ecfd9b71f2f1c
Oksana <ksenia.barshchyk@yandex.ru>
2017-07-27 22:08:06 +0300

    Create special shape using function class and loop

--- mindstorms.py
+++ mindstorms.py
@@ -1,22 +1,28 @@
 import turtle
 
 def draw_square(some_turtle):
-    for i in range(1,5):
-        some_turtle.forward(100)
-        some_turtle.right(90)
+        for i in range(1,5):
+            some_turtle.forward(100)
+            some_turtle.right(90)
+
 
 def draw_art():
     window = turtle.Screen()
     window.bgcolor("red")
     #create the Turtle
     brad = turtle.Turtle()
-    draw_square(brad)
+    brad.speed(2)
+
+    for i in range(1,37):
+        draw_square(brad)
+        brad.right(10)
+
 
     #Create the turtle
-    angie = turtle.Turtle()
-    angie.shape("arrow")
-    angie.color("blue")
-    angie.circle(100)
+    #angie = turtle.Turtle()
+    #angie.shape("arrow")
+    #angie.color("blue")
+    #angie.circle(100)
     window.exitonclick()
 
 draw_art()
